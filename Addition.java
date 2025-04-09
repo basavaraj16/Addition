3416c0420c1afe57037b6118f2d14400ce963214
basavaraj16 <basavarajmugalkhod2005@gmail.com>
2025-04-09 14:26:19 +0530

    save java code to master

--- Addition.java
+++ Addition.java
@@ -2,8 +2,9 @@ class Addition {
     public static void main(String[] args) {
         int a=10;
         int b=5;
-        int sum = a+b;
-        System.out.println("sum is: "+sum);
+        int sub = a-b;
+
+        System.out.println("sub is: "+sub);
     }
 
 }
